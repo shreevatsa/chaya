9af2f0094926201c7c8d6027fcc1f2e754f3dca0
Shreevatsa R <shreevatsa.public@gmail.com>
2024-05-21 21:20:34 -0700

    Also inline CSS. To be backported to release versions.

--- build-inline.py
+++ build-inline.py
@@ -1,8 +1,13 @@
 # In 'ocr.htm', inline `main.js`, to produce `ocr.html`
 js_code = open('main.js', 'r', encoding='utf-8').read()
+css_code = open('main.css', 'r', encoding='utf-8').read()
 html_template = open('ocr.htm', 'r', encoding='utf-8').read()
 html_inlined = html_template.replace(
     r'<script defer type="module" src="main.js"></script>',
     f'<script type="module">{js_code}</script>',
+)
+html_inlined = html_inlined.replace(
+    r'<link rel="stylesheet" type="text/css" href="main.css" />',
+    f'<style>{css_code}</style>',
 )
 open('index.html', 'w', encoding='utf-8').write(html_inlined)
