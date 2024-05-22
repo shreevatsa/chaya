ef135bd87b47fda932d4f699a480baa3eacce078
Shreevatsa R <shreevatsa.public@gmail.com>
2024-05-21 20:58:07 -0700

    Logging on error.

--- functions/releases/[version].js
+++ functions/releases/[version].js
@@ -1,12 +1,18 @@
 export function onRequest(context) {
-    const version = context.params.version;
-    console.log(version);
-    const versionMap = {
-        'chaya-2024.01.html': 'v2024.01',
-    };
-    const subdomain = versionMap[version];
-    if (subdomain) {
-        const newUrl = `https://${subdomain}.scan-cobbler.pages.dev`;
-        return fetch(newUrl);
+    try {
+        const version = context.params.version;
+        console.log(version);
+        const versionMap = {
+            'chaya-2024.01.html': 'v2024-01',
+        };
+        const subdomain = versionMap[version];
+        if (subdomain) {
+            const newUrl = `https://${subdomain}.scan-cobbler.pages.dev/index.html`;
+            console.log(`Fetching from ${newUrl}`);
+            return fetch(newUrl);
+        }
+    } catch (error) {
+        // https://stackoverflow.com/questions/18391212/is-it-not-possible-to-stringify-an-error-using-json-stringify
+        return new Response(JSON.stringify(error, Object.getOwnPropertyNames(error), 2));
     }
 }
