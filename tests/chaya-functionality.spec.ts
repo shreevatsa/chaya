69424141620a11fea9bd4e5a67be336d573eb054
Shreevatsa R <shreevatsa.public@gmail.com>
2025-07-23 22:27:25 -0700

    Revert one of the changes.

--- tests/chaya-functionality.spec.ts
+++ tests/chaya-functionality.spec.ts
@@ -170,9 +170,7 @@ test.describe('Chaya Functionality Tests', () => {
         await page.mouse.move(layerBox!.x + 400, layerBox!.y + 250);
         await page.mouse.up();
 
-        await page.waitForFunction(() =>
-          document.querySelectorAll('.annotation-box').length >= 2
-        );
+        await page.waitForTimeout(1000); // Wait for second annotation to be created
 
         // Save .chaya file
         const downloadPromise = page.waitForEvent('download');
